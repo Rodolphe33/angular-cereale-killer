94a1847e891a59908dd8a2f8ce61fdfc71b186ad
Rodolphe_Lassalle <rlassalle@epsyl-alcen.com>
2024-08-26 12:29:08 +0200

    new prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'kil-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-cereale-killer';
+  title = 'Cereal Killer';
 }
